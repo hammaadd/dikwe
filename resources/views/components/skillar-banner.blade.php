9ccdd644301df40888f79a9f28cd388ed082f227
adilvn <muhammadadil79300@gmail.com>
2021-03-29 15:58:59 +0500

    Added Skillar Banner Component

---
+++ resources/views/components/skillar-banner.blade.php
@@ -0,0 +1,20 @@
+<div class="flex justify-start md:flex-row flex-col gap-4">
+    <div class="relative">
+        <img src="{{ asset('images/Group 521.svg')}}" class="rounded-xl w-full md:w-auto md:h-32"/>
+    </div>
+    <div class="flex flex-col justify-between">
+        <div class="flex items-start my-2 md:m-0">
+            <div class="relative">
+                <a href="https://skillar.com" class="block relative">
+                    <img alt="Skillar Logo" src="{{asset('images/skillar.png')}}" class="h-9"/>
+                </a>
+                <p class="py-3">
+                    Get Your Career Job Now
+                </p>
+            </div>
+        </div>
+        <div class="flex items-start justify-between my-2 md:m-0">
+            <a href="https://skillar.com" class="text-green-550 font-bold w-full text-right">Visit Website <i class="fas fa-arrow-right ml-1"></i></a>
+        </div>
+    </div>
+</div>
