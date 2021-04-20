1e4e6b2e8298a2311812c8ef4f587233d774d82e
Muhammad Adil <muhammadadil79300@gmail.com>
2021-04-20 16:45:48 +0500

    Renamed the Bookmarks Component

--- app/View/Components/bookmarksList.php
+++ app/View/Components/bookmarksList.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class bookmakrsList extends Component
+class bookmarksList extends Component
 {
     /**
      * Create a new component instance.
@@ -23,6 +23,6 @@ class bookmakrsList extends Component
      */
     public function render()
     {
-        return view('components.bookmakrs-list');
+        return view('components.bookmarks-list');
     }
 }
