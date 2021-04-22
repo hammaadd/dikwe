d976db178c17b62f4ad02b96c7ac04bdbf3cbc83
Muhammad Adil <muhammadadil79300@gmail.com>
2021-04-22 18:00:17 +0500

    Added User Network More Info Component

---
+++ resources/views/components/more-info.blade.php
@@ -0,0 +1,46 @@
+<div class="flex flex-wrap overflow-hidden items-center">
+    <div class="w-full overflow-hidden lg:w-1/5 pl-5">
+        <div class="block relative h-24 w-24">
+            <img id="profileImage" alt="profil" src="{{ asset('images/Ellipse 1792x.png') }}" class="object-cover rounded-full w-full h-full"/>
+        </div>
+    </div>
+    <div class="w-full overflow-hidden lg:w-2/5">
+        <span class="block font-bold text-lg">Yomna Sabry</span>
+        <span class="block font-bold text-lg">UI UX Designer</span>
+    </div>
+</div>
+<div class="mt-8">
+    <label for="social-links" class="pl-5">Location</label>
+    <div class="px-5 mt-1 mb-5 user-social-links">
+        <div class=" bg-green-150 rounded-xl p-5">
+            <div class=" w-full">
+                <p class="">Egypt, Cairo, Elmady</p>
+            </div>
+        </div>
+    </div>
+    <label for="social-links" class="pl-5">Location</label>
+    <div class="px-5 mt-1 mb-5 user-social-links">
+        <div class=" bg-green-150 rounded-xl p-5">
+            <div class=" w-full">
+                <p class="">Egypt, Cairo, Elmady</p>
+            </div>
+        </div>
+    </div>
+    <label for="social-links" class="pl-5">Social Links</label>
+    <div class="px-5 mt-1 mb-5 user-social-links">
+        <div class=" bg-green-150 rounded-xl p-5">
+            <div class=" w-full">
+                <p class="info-link"><i class="fab fa-twitter text-green-550"></i><a href="#" class="pl-5">www.twitter.com</a></p>
+            </div>
+            <div class=" w-full">
+                <p class="info-link"><i class="fab fa-facebook-f text-green-550"></i><a href="#" class="pl-5">www.facebook.com</a></p>
+            </div>
+            <div class=" w-full">
+                <p class="info-link"><i class="fab fa-linkedin-in text-green-550"></i><a href="#" class="pl-5">www.linkedin.com</a></p>
+            </div>
+            <div class=" w-full">
+                <p class="info-link"><i class="fas fa-globe text-green-550"></i><a href="#" class="pl-5">www.website.com</a></p>
+            </div>
+        </div>
+    </div>
+</div>
