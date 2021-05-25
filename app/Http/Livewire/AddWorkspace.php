483b7cc1b9ec42510a221c85a85bcbe6d5638316
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-05-25 14:47:08 +0500

    Workspace Add Form

--- app/Http/Livewire/AddWorkspace.php
+++ app/Http/Livewire/AddWorkspace.php
@@ -2,10 +2,48 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Workspace;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class AddWorkspace extends Component
 {
+    use AuthorizesRequests;
+    public $name , $description , $color , $visibility, $workspace;
+
+    private function resetCreateForm(){
+        $this->name = '';
+        $this->description = '';
+    }
+    
+    public function store()
+    {
+        $pretty_names = [
+            'name'  =>  'workspace name',
+        ];
+        
+        $this->validate([
+            'name' => 'required',
+            'description' => 'max:500',
+            'color' => 'required|in:purple,yellow,blue,green',
+            'visibility' => 'required',
+            'workspace'=>'required'
+        ],[],$pretty_names);
+        
+        $wrkspc = new Workspace;
+        $wrkspc->name = $this->name;
+        $wrkspc->description = $this->description;
+        $wrkspc->parent = $this->workspace;
+        $wrkspc->visibility = $this->visibility;
+        $wrkspc->user_id = Auth::id();
+        $wrkspc->save();
+        session()->flash('success', 'Workspace Added Successfully.');
+        // $this->emit('updateTags');
+
+        $this->resetCreateForm();
+    }
+
     public function render()
     {
         return view('livewire.add-workspace');
--- resources/views/livewire/add-workspace.blade.php
+++ resources/views/livewire/add-workspace.blade.php
@@ -10,53 +10,79 @@
     </div>
     <div class="p-2 md:p-8">
         <div>
-            <form action="">
+            <form wire:submit.prevent="store">
                 <input type="text" placeholder="Workspace Name" class="input--field" name="name">
+                @error('name')
+                                <small class="field-error-message">
+                                    <span>{{$message}}</span>
+                                </small>
+                    @enderror
                 <textarea rows="3" class="input--field" placeholder="Workspace Description" name="description"></textarea>
+                @error('description')
+                                <small class="field-error-message">
+                                    <span>{{$message}}</span>
+                                </small>
+                    @enderror
                 <div class="input--field">
+                    <label for="workspace">Select Workspace</label>
                     <select class="multiple-select" id="SelectTag" multiple="multiple" name="workspace" placeholder="Select Parent Workspace">
                         <option value="0" selected>None</option>
                         <option value="">Child Child Workspace</option>
                     </select>
+                    @error('workspace')
+                                <small class="field-error-message">
+                                    <span>{{$message}}</span>
+                                </small>
+                    @enderror
                 </div>
                 <div class="flex flex-wrap overflow-hidden flex-row items-center justify-between">
                     <div class="input--f">
                         <div class=" mx-auto">
                             Color
                             <div class=" inline-block ml-4">
-                                <input type="radio" name="color" class="form-checkbox text-purple-900 border-purple-900 bg-purple-900 rounded-full focus:ring-0" />
-                                <input type="radio" name="color" class="form-checkbox text-yellow-400 border-yellow-400 bg-yellow-400 rounded-full focus:ring-0" />
-                                <input type="radio" name="color" class="form-checkbox text-indigo-700 border-indigo-700 bg-indigo-700 rounded-full focus:ring-0" />
-                                <input type="radio" name="color" class="form-checkbox text-green-550 border-green-550 bg-green-550 rounded-full focus:ring-0" />
+                                <input type="radio" wire:model.defer="color" checked name="color" class="form-checkbox text-purple-900 border-purple-900 bg-purple-900 rounded-full focus:ring-0" value="purple" />
+                                <input type="radio" wire:model.defer="color" name="color" class="form-checkbox text-yellow-400 border-yellow-400 bg-yellow-400 rounded-full focus:ring-0" value="yellow"/>
+                                <input type="radio" wire:model.defer="color" name="color" class="form-checkbox text-indigo-700 border-indigo-700 bg-indigo-700 rounded-full focus:ring-0" value="blue"/>
+                                <input type="radio" wire:model.defer="color" name="color" class="form-checkbox text-green-550 border-green-550 bg-green-550 rounded-full focus:ring-0" value="green"/>
                             </div>
                         </div>
+                        @error('color')
+                                <small class="field-error-message">
+                                    <span>{{$message}}</span>
+                                </small>
+                        @enderror
                     </div>
-                    <div class="input--f">
+                    {{-- <div class="input--f">
                         <i class="fas fa-thumbs-up text-green-550 text-2xl mx-2"></i>
                         <i class="fas fa-thumbs-down text-green-550 text-2xl mx-2"></i>
-                    </div>
+                    </div> --}}
                 </div>
                 <div class="input--field">
                     <div class="flex flex-row justify-around xl:justify-center">
                         <label class="items-center">
-                            <input type="radio" name="rate" class="h-4 w-4 text-green-550 focus:ring-0"/>
+                            <input type="radio" wire:model.defer="visibility" name="visibility" checked class="h-4 w-4 text-green-550 focus:ring-0" value="P"/>
                             <span class="sm:ml-2">
                                 Public
                             </span>
                         </label>
                         <label class="items-center ml-1 sm:ml-4">
-                            <input type="radio" name="rate" class="h-4 w-4 text-green-550 focus:ring-0"/>
+                            <input type="radio" wire:model.defer="visibility" name="visibility" class="h-4 w-4 text-green-550 focus:ring-0" value="R"/>
                             <span class="sm:ml-2">
                                 Restricted
                             </span>
                         </label>
                         <label class="items-center ml-1 sm:ml-4">
-                            <input type="radio" name="rate" class="h-4 w-4 text-green-550 focus:ring-0"/>
+                            <input type="radio" wire:model.defer="visibility" name="visibility" class="h-4 w-4 text-green-550 focus:ring-0" value="PR"/>
                             <span class="sm:ml-2">
                                 Private
                             </span>
                         </label>
                     </div>
+                    @error('visibility')
+                                <small class="field-error-message">
+                                    <span>{{$message}}</span>
+                                </small>
+                    @enderror
                 </div>
                 <div class="text-center md:text-right my-4">
                     <button class="btn-gray">Cancel</button>
