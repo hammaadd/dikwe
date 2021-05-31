9a8aa5f8950486bd3357914864e40950c7b23f4b
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-05-31 14:42:27 +0500

    Added Sluggable Package

--- app/Models/WorkSpace.php
+++ app/Models/WorkSpace.php
@@ -5,11 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Workspace extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    use Sluggable;
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
     
     public function parent()
     {
