481543f8bea547a056aa0285c3e06bcdce37d871
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-06-14 16:40:06 +0500

    Captcha Integration Completed

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -57,7 +57,7 @@ class RegisterController extends Controller
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'captcha' => ['required','captcha']
-        ]);
+        ],['captcha.captcha'=>'Invalid captcha try again.']);
     }
 
     /**
