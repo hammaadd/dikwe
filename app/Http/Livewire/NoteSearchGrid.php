3d0195e22f2a8174e60aa5363d65dee91e0e6226
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-06-21 21:44:32 +0500

    Pagination added in Livewire

--- app/Http/Livewire/NoteSearchGrid.php
+++ app/Http/Livewire/NoteSearchGrid.php
@@ -5,37 +5,23 @@ namespace App\Http\Livewire;
 use App\Models\Note;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class NoteSearchGrid extends Component
 {
 
-    
+    use WithPagination;
     protected $listeners = ['updateNoteSet'=> 'updateSet','setNoteStyle'=>'setNoteStyle'];
-    public $notes, $note_set, $noteStyle , $noteHeading ;
+    public  $note_set, $noteStyle , $noteHeading ;
+    private $notes;
+    
     public function render()
     {
-        return view('livewire.note-search-grid');
-    }
-
-    public function mount(){
-        $this->notes = Note::where('status','active')
-                        ->where('created_by',Auth::id())
-                        ->orderBy('created_at','DESC')
-                        ->limit(4)
-                        ->get();
-        $this->noteStyle = 'grid';
-        $this->noteHeading = 'My Notes';
-    }
-
-    public function updateSet($noteSet){
-        $this->note_set = $noteSet;
-        //dd($this->note_set);
         if($this->note_set == 'M'):
             $this->notes = Note::where('status','active')
                         ->where('created_by',Auth::id())
                         ->orderBy('created_at','DESC')
-                        ->limit(6)
-                        ->get();
+                        ->paginate(4);
             $this->noteHeading = 'My Notes';
 
         elseif($this->note_set == 'S'):
@@ -45,19 +31,34 @@ class NoteSearchGrid extends Component
         elseif($this->note_set == 'SR'):
             $this->notes = Note::where('status','active')
                         ->orderBy('created_at','ASC')
-                        ->limit(6)
-                        ->get();
+                        ->paginate(4);
             $this->noteHeading = 'Service Notes';
 
         else:
             $this->notes = Note::where('status','active')
                         ->where('created_by',Auth::id())
                         ->orderBy('created_at','DESC')
-                        ->limit(6)
-                        ->get();
+                        ->paginate(4);
             $this->noteHeading = 'My Notes';
 
         endif;
+
+        return view('livewire.note-search-grid',['notes' => $this->notes]);
+    }
+
+    public function mount(){
+        $this->notes = Note::where('status','active')
+                        ->where('created_by',Auth::id())
+                        ->orderBy('created_at','DESC')
+                        ->paginate(4);
+        $this->noteStyle = 'grid';
+        $this->noteHeading = 'My Notes';
+    }
+
+    public function updateSet($noteSet){
+        $this->note_set = $noteSet;
+        //dd($this->note_set);
+        
     }
 
     public function setNoteStyle($style){
