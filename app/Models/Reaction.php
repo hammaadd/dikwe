9541e3956d9db71857a95cae83084910686fa85f
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-06-24 09:56:42 +0500

    Reaction model Added

--- app/Models/Reaction.php
+++ app/Models/Reaction.php
@@ -8,8 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Reaction extends Model
 {
     use HasFactory;
+    protected $guarded = [];
 
     public function note(){
         return $this->belongsTo(Note::class,'ka');
+    }
+
+    
+
+    public function activeNote(){
+        return $this->note->where('status','active');
     }
 }
