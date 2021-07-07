d0c741a0d97e7939b494d5e068a3a883baf11642
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-07-07 10:10:01 +0500

    Create NoteView.php

---
+++ app/Http/Livewire/NoteView.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class NoteView extends Component
+{
+    public function render()
+    {
+        return view('livewire.note-view');
+    }
+}
