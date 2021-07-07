162a2157fca5717c70ddf09718dd7a7e971c9937
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-07-07 10:10:24 +0500

    Added Dispatch alpine function

--- resources/views/livewire/edit-note.blade.php
+++ resources/views/livewire/edit-note.blade.php
@@ -3,7 +3,7 @@
     <div class="flex flex-wrap justify-between relative">
         <div class="bg-green-550 text-white font-bold px-2 md:px-8 py-1 md:py-3 br-top-left"><label for="note"> <i class="fas fa-cog mr-2"></i>Edit Note Info</label></div>
         <div class="py-3 px-2 md:px-8 lg:px-2 xl:px-8 hidden sm:block">
-            <a href="javascript:void(0)" x-on:click="nshowAdd = true, nshowEdit = false" class="link-hover text-green-550 font-bold">
+            <a href="javascript:void(0)" x-on:click="$dispatch('showsearchnote')" class="link-hover text-green-550 font-bold">
                 Back To The Notes
             </a>
         </div>
