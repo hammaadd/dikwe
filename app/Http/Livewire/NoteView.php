be9f152f5ebe0ca84d8f064749eca2d04d732c45
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-07-08 09:35:54 +0500

    View Note Function

--- app/Http/Livewire/NoteView.php
+++ app/Http/Livewire/NoteView.php
@@ -6,8 +6,17 @@ use Livewire\Component;
 
 class NoteView extends Component
 {
+    public $noteId;
     public function render()
     {
         return view('livewire.note-view');
     }
+
+    public function show($id){
+        $this->dispatchBrowserEvent('showView');
+        $this->noteId = $id;
+    }
+
+
+
 }
