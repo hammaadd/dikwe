aaf3f7311f9fb3a61fe739731381db3caf859755
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-07-08 09:38:01 +0500

    Update notes-search.blade.php

--- resources/views/user/inc/notes-search.blade.php
+++ resources/views/user/inc/notes-search.blade.php
@@ -13,7 +13,7 @@
                         <div class="flex flex-wrap justify-between relative">
                             <div class="bg-green-550 text-white font-bold px-2 md:px-8 lg:px-2 xl:px-8 py-3 br-top-left"><label for="knowledge-assets">Notes</label></div>
                         </div>
-                        <div class="w-full lg:w-3/4 xl:w-3/4 mx-auto py-5 px-2">
+                        {{-- <div class="w-full lg:w-3/4 xl:w-3/4 mx-auto py-5 px-2">
                             <form action="" class="flex flex-col text-center">
                                 <div class="relative rounded-xl">
                                     <input type="text" name="name" class="input-search" placeholder="Search Any Notes"/>
@@ -22,14 +22,17 @@
                                             <i class="text-white fas fa-search"></i>
                                         </span>
                                     </button>
-                                    {{-- <div class="absolute bg-green-150 w-full h-40 z-50 top-full mt-2 rounded-xl shadow-md"></div> --}}
+                                    <div class="absolute bg-green-150 w-full h-40 z-50 top-full mt-2 rounded-xl shadow-md"></div>
                                 </div>
                             </form>
-                        </div>
+                        </div> --}}
                         
-                        <div class="w-full px-2 md:px-5 lg:px-2 xl:px-5 flex flex-wrap justify-between relative" >
-                            <div class="mx-auto md:mx-0">
-                            </div>
+                        
+                            <div class="w-full px-2 md:px-5 flex flex-wrap justify-between items-center relative mt-5">
+                                <a  href="javascript:void(0)" x-on:click="$dispatch('showaddform')" wire:click="moreInfo" title="Add note" class="bg-green-550 text-white font-bold py-2 px-3 mx-2 rounded-xl border-2 border-green-550 hover:bg-white hover:text-green-550 focus:outline-none">
+                                    <i class="fas fa-plus-circle"></i>
+                                </a>
+                            
                             <livewire:note-search-grid-buttons/>
                             <ul x-show="isOpen"
                                 @click.away="isOpen = false"
@@ -193,6 +196,8 @@
                                 </div>
                             </form>
                         </div>
+
+                        
 
 
                         <livewire:note-search-grid :key="rand()"/>
