d5a0fd47d1f6e461ca07870bab5ce12eb882882f
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-07-09 09:58:11 +0500

    Note View Function added

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use Illuminate\Http\Request;
 
 class NoteController extends Controller
@@ -10,5 +11,18 @@ class NoteController extends Controller
 
     public function index(Request $request){
         return view('user.content.notes');
+    }
+
+    public function show(Request $request, $id){
+        if(isset($id)){
+            $note = Note::find($id);
+            return view('user.note.show',['note'=>$note]);
+        }else{
+            return back();
+        }
+    }
+
+    public function checkTest(){
+        
     }
 }
