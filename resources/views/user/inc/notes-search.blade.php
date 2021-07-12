9469b4a95b00e5fdeb97bb12801fa0c72521a1db
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-07-12 10:32:30 +0500

    Update notes-search.blade.php

--- resources/views/user/inc/notes-search.blade.php
+++ resources/views/user/inc/notes-search.blade.php
@@ -9,201 +9,21 @@
             >
                     <!-- Column Content -->
                     {{-- Tag Section --}}
-                <div class="bg-white pb-5 rounded-xl h-full mt-4 lg:mt-0">
-                        <div class="flex flex-wrap justify-between relative">
+            <div class="bg-white pb-5 rounded-xl h-full mt-4 lg:mt-0">
+                <div class="flex flex-wrap justify-between relative">
                             <div class="bg-green-550 text-white font-bold px-2 md:px-8 lg:px-2 xl:px-8 py-3 br-top-left"><label for="knowledge-assets">Notes</label></div>
-                        </div>
-                        {{-- <div class="w-full lg:w-3/4 xl:w-3/4 mx-auto py-5 px-2">
-                            <form action="" class="flex flex-col text-center">
-                                <div class="relative rounded-xl">
-                                    <input type="text" name="name" class="input-search" placeholder="Search Any Notes"/>
-                                    <button class="absolute inset-y-0 right-0 px-4 flex items-center bg-green-550 rounded-xl">
-                                        <span class="text-xl">
-                                            <i class="text-white fas fa-search"></i>
-                                        </span>
-                                    </button>
-                                    <div class="absolute bg-green-150 w-full h-40 z-50 top-full mt-2 rounded-xl shadow-md"></div>
-                                </div>
-                            </form>
-                        </div> --}}
-                        
-                        
-                            <div class="w-full px-2 md:px-5 flex flex-wrap justify-between items-center relative mt-5">
-                                <a  href="javascript:void(0)" x-on:click="$dispatch('showaddform')" wire:click="moreInfo" title="Add note" class="bg-green-550 text-white font-bold py-2 px-3 mx-2 rounded-xl border-2 border-green-550 hover:bg-white hover:text-green-550 focus:outline-none">
-                                    <i class="fas fa-plus-circle"></i>
-                                </a>
-                            
-                            <livewire:note-search-grid-buttons/>
-                            <ul x-show="isOpen"
-                                @click.away="isOpen = false"
-                                x-transition:enter="transition transform origin-top-right ease-out duration-200"
-                                x-transition:enter-start="opacity-0 scale-75"
-                                x-transition:enter-end="opacity-100 scale-100"
-                                x-transition:leave="transition transform origin-top-right ease-out duration-200"
-                                x-transition:leave-start="opacity-100 scale-100"
-                                x-transition:leave-end="opacity-0 scale-75"
-                                class="absolute bg-white shadow-md overflow-hidden rounded-xl w-72 mt-2 py-1 right-0 top-full md:right-10 md:top-10 z-20"
-                            >
-                                    <li>
-                                        <a href="#" class="dropdown-item">
-                                            <i class="fas fa-plus-circle dropdown-item-icon"></i>
-                                            <span class="ml-2">Add New Bookmark Here</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="dropdown-item">
-                                            <i class="fas fa-external-link-alt dropdown-item-icon"></i>
-                                            <span class="ml-2">Open All Bookmarks</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="dropdown-item">
-                                            <i class="fas fa-share-alt dropdown-item-icon"></i>
-                                            <span class="ml-2">Share Bookmarks</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="dropdown-item">
-                                            <i class="fas fa-tags dropdown-item-icon"></i>
-                                            <span class="ml-2">Tag Bookmarks</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="dropdown-item">
-                                            <i class="fas fa-tag dropdown-item-icon"></i>
-                                            <span class="ml-2">Untag Bookmarks</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="dropdown-item">
-                                            <i class="fas fa-lock dropdown-item-icon"></i>
-                                            <span class="ml-2">Make Bookmarks Private</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="dropdown-item">
-                                            <i class="fas fa-globe-americas dropdown-item-icon"></i>
-                                            <span class="ml-2">Make Bookmarks Public</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="dropdown-item">
-                                            <i class="fas fa-users dropdown-item-icon"></i>
-                                            <span class="ml-2">Make Bookmarks Restricted</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="dropdown-item">
-                                            <i class="fas fa-trash-alt dropdown-item-icon"></i>
-                                            <span class="ml-2">Delete Bookmarks</span>
-                                        </a>
-                                    </li>
-                            </ul>
-                            <form action=""
-                                x-show="fOpen"
-                                @click.away="fOpen = false"
-                                x-transition:enter="transition transform origin-top-right ease-out duration-200"
-                                x-transition:enter-start="opacity-0 scale-75"
-                                x-transition:enter-end="opacity-100 scale-100"
-                                x-transition:leave="transition transform origin-top-right ease-out duration-200"
-                                x-transition:leave-start="opacity-100 scale-100"
-                                x-transition:leave-end="opacity-0 scale-75"
-                                class="absolute bg-white shadow-md overflow-hidden rounded-xl w-72 mt-2 p-5 right-0 top-full md:right-20 md:top-10 z-20">
-                                <p class="font-bold text-center">Filter By</p>
-                                <div class="flex flex-col border-b border-gray-500 py-6">
-                                    <label class="items-center">
-                                        <input type="radio" name="rate" class="h-4 w-4 text-green-550 focus:ring-0"/>
-                                        <span class="ml-2">
-                                            High Rate
-                                        </span>
-                                    </label>
-                                    <label class="items-center">
-                                        <input type="radio" name="rate" class="h-4 w-4 text-green-550 focus:ring-0"/>
-                                        <span class="ml-2">
-                                            Low Rate
-                                        </span>
-                                    </label>
-                                    <label class="items-center">
-                                        <input type="radio" name="rate" class="h-4 w-4 text-green-550 focus:ring-0"/>
-                                        <span class="ml-2">
-                                            All
-                                        </span>
-                                    </label>
-                                </div>
-                                <div class="flex flex-col border-b border-gray-500 py-6">
-                                    <label class="items-center">
-                                        <input type="radio" name="clicks" class="h-4 w-4 text-green-550 focus:ring-0"/>
-                                        <span class="ml-2">
-                                            High Number of Clicks
-                                        </span>
-                                    </label>
-                                    <label class="items-center">
-                                        <input type="radio" name="clicks" class="h-4 w-4 text-green-550 focus:ring-0"/>
-                                        <span class="ml-2">
-                                            Low Number of Clicks
-                                        </span>
-                                    </label>
-                                    <label class="items-center">
-                                        <input type="radio" name="clicks" class="h-4 w-4 text-green-550 focus:ring-0"/>
-                                        <span class="ml-2">
-                                            All
-                                        </span>
-                                    </label>
-                                </div>
-                                <div class="flex flex-col border-b border-gray-500 py-6">
-                                    <label class="items-center">
-                                        <input type="radio" name="shares" class="h-4 w-4 text-green-550 focus:ring-0"/>
-                                        <span class="ml-2">
-                                            High Number of Shares
-                                        </span>
-                                    </label>
-                                    <label class="items-center">
-                                        <input type="radio" name="shares" class="h-4 w-4 text-green-550 focus:ring-0"/>
-                                        <span class="ml-2">
-                                            Low Number of Shares
-                                        </span>
-                                    </label>
-                                    <label class="items-center">
-                                        <input type="radio" name="shares" class="h-4 w-4 text-green-550 focus:ring-0"/>
-                                        <span class="ml-2">
-                                            All
-                                        </span>
-                                    </label>
-                                </div>
-                                <div class="flex flex-col py-6">
-                                    <label class="items-center">
-                                        <input type="radio" name="time" class="h-4 w-4 text-green-550 focus:ring-0"/>
-                                        <span class="ml-2">
-                                            Newest
-                                        </span>
-                                    </label>
-                                    <label class="items-center">
-                                        <input type="radio" name="time" class="h-4 w-4 text-green-550 focus:ring-0"/>
-                                        <span class="ml-2">
-                                            Oldest
-                                        </span>
-                                    </label>
-                                    <label class="items-center">
-                                        <input type="radio" name="time" class="h-4 w-4 text-green-550 focus:ring-0"/>
-                                        <span class="ml-2">
-                                            All
-                                        </span>
-                                    </label>
-                                </div>
-                                <div class=" text-right">
-                                    <button @click=" fOpen = !fOpen " class="bg-gray-400 text-white font-bold px-4 py-1 mx-2 rounded-xl focus:outline-none">Cancel</button>
-                                    <button @click=" fOpen = !fOpen " type="submit" class="bg-green-550 text-white font-bold px-4 py-1 mx-2 rounded-xl focus:outline-none">Apply</button>
-                                </div>
-                            </form>
-                        </div>
-
-                        
-
-
-                        <livewire:note-search-grid :key="rand()"/>
-
-
                 </div>
+                <div class="w-full px-2 md:px-5 flex flex-wrap justify-between items-center relative mt-5">
+                    <a  href="javascript:void(0)" x-on:click="$dispatch('showaddform')" wire:click="moreInfo" title="Add note" class="bg-green-550 text-white font-bold py-2 px-3 mx-2 rounded-xl border-2 border-green-550 hover:bg-white hover:text-green-550 focus:outline-none">
+                        <i class="fas fa-plus-circle"></i>
+                    </a>
+                
+                    <livewire:note-search-grid-buttons/>
+                    <livewire:note-select/>
+                </div>
+                        
+                <livewire:note-search-grid :key="rand()"/>
+            </div>
 </div>
 
 
