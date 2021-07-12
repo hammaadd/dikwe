34b07ae7810490cac5c5e665f855b6f2a9b3cc67
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-07-12 10:19:06 +0500

    All check functionality added

--- app/Http/Livewire/NoteFilter.php
+++ app/Http/Livewire/NoteFilter.php
@@ -2,17 +2,29 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Note;
 use Livewire\Component;
 
 class NoteFilter extends Component
 {
+    protected $listeners = ['resetSearch'=>'resetSearch'];
 
-    public $visi_type,$color,$notes_set = 'M';
+    public $visi_type,$color,$notes_set = 'M', $search,$results,$isVisible, $all_checked;
+
+    
     public function mount(){
         $this->visi_type = 'A';
         $this->color = 'A';
         $this->notes_set = 'M';
+        $this->isVisible = false;
+        $this->all_checked = false;
+        $this->resetSearch();
     }
+
+    public function resetSearch() {  
+        $this->search = '';
+        $this->results = [];
+}
 
     public function render()
     {
@@ -35,6 +47,17 @@ class NoteFilter extends Component
         $this->notes_set = $notesSet;
         $this->emit('updateNoteSet',$this->notes_set);
     }
+
+    public function updatedSearch() {
+        if(!empty($this->search)){
+            $this->results = Note::where('title','like',$this->search.'%')->take(5)->get();
+            $results2 = Note::where('title','like','%'.$this->search.'%')->take(5)->get();
+            $this->results = $this->results->merge($results2);
+            $this->isVisible = true;
+        }
+    }
+
+    
 
     
 
