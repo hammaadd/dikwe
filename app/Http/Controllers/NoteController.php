d81484db275c9544f210639332f8c5ee21586e99
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-07-15 10:34:52 +0500

    Update NoteController.php

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Note;
+use App\Models\Practice;
 use Illuminate\Http\Request;
 
 class NoteController extends Controller
@@ -16,6 +17,9 @@ class NoteController extends Controller
     public function show(Request $request, $id){
         if(isset($id)){
             $note = Note::find($id);
+            if(!$note){
+                return back();
+            }
             return view('user.note.show',['note'=>$note]);
         }else{
             return back();
@@ -25,4 +29,35 @@ class NoteController extends Controller
     public function checkTest(){
         
     }
+
+    // public function practice(Request $request){
+    //    if($request){
+
+    //        $data = new Practice;
+    //        $data->word1 = $request->word1;
+    //        $data->word2 = $request->word2;
+    //        $data->word3 = $request->word3;
+    //        $data->word4 = $request->word4;
+    //        $data->def1 = $request->def1;
+    //        $data->def2 = $request->def2;
+    //        $data->def3 = $request->def3;
+    //        $data->def4 = $request->def4;
+    //        $res = $data->save();
+
+    //        if($res){
+    //            $message['success'] = "Cards created Successfully";
+               
+    //        }else{
+    //             $message['error'] = "Not able to create card try later!";
+                
+    //        }
+    //        return $message;
+    //    }
+    // }
+
+    // public function fetchData(){
+    //      $data = Practice::latest()->first();
+
+    //      return $data;
+    // }
 }
