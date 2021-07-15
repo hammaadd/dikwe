c0b92d9ba6efb2d9760ac4bc7340377ab7fbf1cb
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-07-15 10:35:42 +0500

    Create new workspace & Tag logic updated

--- app/Http/Livewire/AddNote.php
+++ app/Http/Livewire/AddNote.php
@@ -5,20 +5,22 @@ namespace App\Http\Livewire;
 use App\Models\Note;
 use App\Models\NoteTag;
 use App\Models\NoteWorkspace;
+use App\Models\Tag;
+use App\Models\Workspace;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class AddNote extends Component
 {
     public $color,$title, $description,$source,$url,$visibility,$tagsG,$wrkspcs, $workspaces = [], $tags = [];
-    protected $listeners = ['passMoreInfo'=> 'passMoreInfo'];
+    protected $listeners = ['passMoreInfo'=> 'passMoreInfo','setWorkspaces3'=> 'setWorkspaces','setTags3'=>'setTags'];
     public function render()
     {
         return view('livewire.add-note');
     }
 
     public function moreInfo(){
-        $this->emit('getQuillValue');
+        //$this->emit('getQuillValue');
         $this->emit('showMoreInfo',$this->title, $this->description,$this->color);
     }
 
@@ -70,12 +72,26 @@ class AddNote extends Component
         $nRes = $note->save();
         if($nRes){
             $nsRes = 0;
-            #Add workspaces of notes using loop
             if(count($this->workspaces) > 0):
-                foreach($this->workspaces as $ws){
+            foreach($this->workspaces as $ws){
+                $nws = Workspace::find($ws);
+                    if(!$nws){
+                        $nws = new Workspace;
+                        // $nws->user_id = Auth::id() ;
+                        // $nws->tag = $tg;
+                        // $nws->visibility = 'PR' ;
+                        // $nws->color = 'purple';
+                        // $nws->save();
+
+                        $nws->title = $ws;
+                        $nws->color = 'purple';
+                        $nws->visibility = 'P';
+                        $nws->created_by = Auth::id();
+                        $nws->save();
+                    }
                     $nw = new NoteWorkspace;
-                    $nw->note = $note->id;
-                    $nw->workspace = $ws;
+                    $nw->note = $this->note->id;
+                    $nw->workspace = $nws->id;
                     $nw->save();
                     $nsRes++;
                 }
@@ -85,16 +101,54 @@ class AddNote extends Component
 
             // Add tags of notes using the method below
             if(count($this->tags) > 0):
+                $nts = NoteTag::select('id')->where('note',$this->note->id)->get()->toArray();
+                NoteTag::destroy($nts);
                 foreach($this->tags as $tg){
+                    $taags = Tag::find($tg);
+                    if(!$taags){
+                        $taags = new Tag;
+                        $taags->user_id = Auth::id() ;
+                        $taags->tag = $tg;
+                        $taags->visibility = 'PR' ;
+                        $taags->color = 'purple';
+                        $taags->save();
+                    }
                     $nt = new NoteTag;
-                    $nt->note = $note->id;
-                    $nt->tag = $tg;
+                    $nt->note = $this->note->id;
+                    $nt->tag = $taags->id;
                     $nt->save();
                     $nsRes++;
                 }
             else:
                 $nsRes++;
             endif;
+
+            
+            #Add workspaces of notes using loop
+            // if(count($this->workspaces) > 0):
+            //     foreach($this->workspaces as $ws){
+            //         $nw = new NoteWorkspace;
+            //         $nw->note = $note->id;
+            //         $nw->workspace = $ws;
+            //         $nw->save();
+            //         $nsRes++;
+            //     }
+            // else:
+            //     $nsRes++;
+            // endif;
+
+            // // Add tags of notes using the method below
+            // if(count($this->tags) > 0):
+            //     foreach($this->tags as $tg){
+            //         $nt = new NoteTag;
+            //         $nt->note = $note->id;
+            //         $nt->tag = $tg;
+            //         $nt->save();
+            //         $nsRes++;
+            //     }
+            // else:
+            //     $nsRes++;
+            // endif;
 
             
             
@@ -108,14 +162,26 @@ class AddNote extends Component
 
         
 
-        $this->resetCreateForm();
-        $this->emit('updateNoteGrid');
+        // $this->resetCreateForm();
+        // $this->emit('updateNoteGrid');
         // $this->emit('updateNotes');
+
+        return redirect()->route('notes');
     }
 
     public function cancelForm(){
         $this->resetCreateForm();
         $this->emit('cancelFormInfo');
         return redirect()->route('notes');
+    }
+
+    public function setWorkspaces($wkspcs){
+        $this->workspaces = $wkspcs;
+        
+    }
+
+    public function setTags($tgs){
+        $this->tags = $tgs;
+        
     }
 }
