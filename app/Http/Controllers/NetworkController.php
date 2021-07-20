666a49116c5c94c4500c05929afd901cb8d7e7f3
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-07-20 14:33:41 +0500

    Create NetworkController.php

---
+++ app/Http/Controllers/NetworkController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NetworkController extends Controller
+{
+    //
+
+    public function index(){
+        return view('user.content.network');
+    }
+
+    
+
+
+}
