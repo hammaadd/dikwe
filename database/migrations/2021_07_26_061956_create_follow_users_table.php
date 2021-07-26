2b1494f6cd18e0fbe0cbf160a6e94f9b2d77aa1f
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-07-26 15:58:20 +0500

    Create 2021_07_26_061956_create_follow_users_table.php

---
+++ database/migrations/2021_07_26_061956_create_follow_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('follow_users', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('follow_id')->nullable();
+            $table->foreign('follow_id')->references('id')->on('users')->onDelete('set null');
+            $table->unsignedBigInteger('follower_id')->nullable();
+            $table->foreign('follower_id')->references('id')->on('users')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('follow_users');
+    }
+}
