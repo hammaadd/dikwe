722da2efb302947e135e5046bbf9fadb94641817
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-08-03 17:35:01 +0500

    Update note-search-grid.blade.php

--- resources/views/livewire/note-search-grid.blade.php
+++ resources/views/livewire/note-search-grid.blade.php
@@ -259,5 +259,5 @@
             <img src="{{ asset('assets/loader/three-dots.svg') }}" class="">
         </div>
     </div>
-    @include('user.sections.notification')
+    {{-- @include('user.sections.notification') --}}
     </div>
