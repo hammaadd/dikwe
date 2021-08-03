1152486a7e88a8ef7dccf789255d0246c78fa4ab
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-08-03 17:35:26 +0500

    Update header.blade.php

--- resources/views/user/inc/header.blade.php
+++ resources/views/user/inc/header.blade.php
@@ -8,10 +8,7 @@
     <link href="{{ asset('css/user.min.css') }}" rel="stylesheet">
     <!-- Font Awesome CSS -->
     <link href="{{ asset('css/all.min.css') }}" rel="stylesheet">
-    
-    {{-- <script src="https://cdn.jsdelivr.net/npm/turbolinks@5.2.0/dist/turbolinks.min.js"></script> --}}
     @include('user.inc.styles.fonts')
     @yield('headerExtra')
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
-    {{-- <script src="https://cdn.jsdelivr.net/gh/alpine-collective/alpine-magic-helpers@1.2.x/dist/index.min.js" defer></script> --}}
 </head>
