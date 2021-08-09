ff5ff42d73e0969884fcb3b28cb516bb2e93189e
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-08-09 18:23:37 +0500

    Twitter Card Tags added

--- resources/views/user/inc/header.blade.php
+++ resources/views/user/inc/header.blade.php
@@ -3,7 +3,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta property="fb:app_id" content="995443947955637"/>
     @yield('openGraph_fb')
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@dikwe" />
+    <meta name="twitter:creator" content="@dikwe" />
     <title>@yield('title') - DIKWE</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/user.min.css') }}" rel="stylesheet">
