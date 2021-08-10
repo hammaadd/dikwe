3f71812cf8e83376e9d00a866c3d3ced52f7258a
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-08-10 17:28:33 +0500

    Update search-network.blade.php

--- resources/views/livewire/search-network.blade.php
+++ resources/views/livewire/search-network.blade.php
@@ -21,30 +21,8 @@
             <div class="w-full flex flex-wrap items-center overflow-hidden">
                 <!-- Column Content -->
                 @forelse($results as $user)
-                    <div class="w-full py-2 flex flex-wrap items-center justify-between rounded-xl">
-                        <div class="flex flex-wrap items-center">
-                            <div class="w-full flex items-center">
-                                <a href="{{route('u.profile.detail',$user)}}" class="block relative">
-                                    <img alt="User Image" src="@if($user->profile_img == null) https://ui-avatars.com/api/?background=FFFFFF&name={{ str_replace(' ','+' ,$user->name) }} @else {{asset('user_profile_images/'.$user->profile_img)}} @endif"
-                                    class="mx-auto object-cover rounded-full h-12 w-12 lg:h-10 xl:h-12 lg:w-10 xl:w-12 border-2 border-green-550">
-                                </a>
-                                <div class="flex flex-col items-start ml-2">
-                                    <a href="{{route('u.profile.detail',$user)}}" class="hover:text-green-550">
-                                        {{$user->name}}
-                                    </a>
-                                    <span class="text-sm font-bold">
-                                        <i class="fas fa-map-marker-alt"></i>@isset($user->country->country) {{$user->country->country}} @endisset
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="flex flex-wrap items-center">
-                            <button class=" bg-green-150 text-gray-400 text-sm text-center rounded-full px-2 py-1 mx-3 lg:mx-0 xl:mx-3 hover:bg-green-550 hover:text-white">
-                                <livewire:follow-unfollow-user :user="$user" :key="$user->id" />
-                            </button>
-                        </div>
-                    </div>
-                    <hr class="text-gray-500">
+                    <livewire:network-users :user="$user" :wire:key="$user->id"/>
+                    {{-- <hr class="text-gray-500"> --}}
                 @empty
                     <div class=" text-left p-3"><span class="font-light text-red-600">No result found!</span></div>
                 @endforelse
