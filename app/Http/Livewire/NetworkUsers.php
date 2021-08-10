d44cf1e83f1d86a52b2c49ad5d6f32ae4e0cc24e
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-08-10 17:28:06 +0500

    Create NetworkUsers.php

---
+++ app/Http/Livewire/NetworkUsers.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\FollowUser;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+class NetworkUsers extends Component
+{
+    public $user;
+    public function mount($user){
+        $this->user = $user;
+    }
+    public function render()
+    {
+        return view('livewire.network-users');
+    }
+
+    public function followUser($fId){
+        if(Auth::id()){
+
+                FollowUser::updateOrCreate(
+                    ['follow_id'=>$fId,'follower_id'=>Auth::id()]
+                );
+        }
+    }
+
+    public function unfollowUser($fId){
+        if(Auth::id()){
+            FollowUser::where('follow_id',$fId)->where('follower_id',Auth::id())->delete();
+        }
+    }
+}
