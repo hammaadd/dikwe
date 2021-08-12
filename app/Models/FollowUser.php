0c849afa39e4f37b8d575c045e86112b97982d8d
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-08-12 15:22:34 +0500

    Follower and Follow relation created

--- app/Models/FollowUser.php
+++ app/Models/FollowUser.php
@@ -10,6 +10,12 @@ class FollowUser extends Model
     use HasFactory;
     protected $guarded = [];
 
-    
+    public function follower(){
+        return $this->hasMany(User::class,'id');
+    }
+
+    public function follow(){
+        return $this->belongsTo(User::class,'follow_id','id');
+    }
 
 }
