ac46e4d01962cd92f44b21c214c261d997f21938
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-08-12 15:22:09 +0500

    Create ServiceListNote.php

---
+++ app/Http/Livewire/ServiceListNote.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ServiceListNote extends Component
+{
+    public function render()
+    {
+        return view('livewire.service-list-note');
+    }
+}
