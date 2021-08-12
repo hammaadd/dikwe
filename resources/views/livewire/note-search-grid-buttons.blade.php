c6dc5abe40a1a04887e983812c2ed05fdc0f40b1
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-08-12 15:26:29 +0500

    Update note-search-grid-buttons.blade.php

--- resources/views/livewire/note-search-grid-buttons.blade.php
+++ resources/views/livewire/note-search-grid-buttons.blade.php
@@ -1,4 +1,7 @@
-<div class="mt-2 mx-auto md:mx-0">                     
+<div class="mt-2 mx-auto md:mx-0 px-3">
+    {{-- <button x-on:click="$dispatch('showaddform')" wire:click="moreInfo" title="Add note" class="text-green-550 bg-green-150 rounded-xl mx-1 px-2 h-10 w-10 hover:text-green-550 focus:outline-none">
+        <i class="fas fa-plus-circle text-xl align-middle"></i>
+    </button>                --}}
     <button class="text-gray-400 bg-green-150 rounded-xl mx-1 px-2 h-10 w-10 hover:text-green-550 focus:outline-none @if($noteStyle=='list') text-green-550 @endif" 
     
     wire:click="updateNoteStyle('list')"
