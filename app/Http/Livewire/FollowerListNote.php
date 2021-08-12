35c6b0cc42b3ab74aac11d764f83a2b68e44c73e
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-08-12 09:45:49 +0500

    Create FollowerListNote.php

---
+++ app/Http/Livewire/FollowerListNote.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class FollowerListNote extends Component
+{
+    public function render()
+    {
+        return view('livewire.follower-list-note');
+    }
+}
