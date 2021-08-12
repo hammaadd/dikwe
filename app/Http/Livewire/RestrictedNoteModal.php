00bd246af1457a0880f2fa18a7ace1d74b373200
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-08-12 15:21:56 +0500

    Create RestrictedNoteModal.php

---
+++ app/Http/Livewire/RestrictedNoteModal.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\FollowUser;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class RestrictedNoteModal extends Component
+{
+    use WithPagination;
+    private $users;
+    public function render()
+    {
+        return view('livewire.restricted-note-modal',['users'=>$this->users]);
+    }
+
+    public function mount(){
+        // $this->users = Auth::user()->following;
+        //dd($this->users[0]->follow);
+        $this->users = FollowUser::where('follower_id',Auth::id())->paginate(4);
+        //dd($this->users[0]->follow);
+    }
+
+    public function refereshFollowing(){
+        $this->emit('refereshNoteFollowing');
+        
+    }
+
+
+
+
+}
