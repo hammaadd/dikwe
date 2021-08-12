75434f9d3c9b757bd1e70cb89df59831f683d089
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-08-12 15:21:43 +0500

    Create FollowingListNote.php

---
+++ app/Http/Livewire/FollowingListNote.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\FollowUser;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+use Livewire\WithPagination;
+class FollowingListNote extends Component
+{
+    use WithPagination;
+    protected $listeners =['refereshNoteFollowing'=>'refereshNoteFollowing'];
+    private $users;
+    public $addedUsers = [];
+    public function render()
+    {
+        $this->users = FollowUser::where('follower_id',Auth::id())->paginate(5);
+        return view('livewire.following-list-note',['users'=>$this->users]);
+    }
+
+    // public function mount(){
+    //     $this->users = FollowUser::where('follower_id',Auth::id())->paginate(1);
+    // }
+
+    public function refereshNoteFollowing(){
+        $this->mount();
+    }
+
+    public function addUser($id){
+        if(!in_array($id, $this->addedUsers, true)){
+            array_push($this->addedUsers,$id);
+            $this->dispatchBrowserEvent('alert', ['type' => 'success', 'message' => 'User added to list.']);
+        }else{
+            $this->dispatchBrowserEvent('alert', ['type' => 'error', 'message' => 'Unable to add to list.']);
+        }
+
+
+    }
+
+    public function removeUser($id){
+        if(in_array($id, $this->addedUsers, true)){
+            if (($key = array_search($id, $this->addedUsers)) !== false) {
+                unset($this->addedUsers[$key]);
+                $this->dispatchBrowserEvent('alert', ['type' => 'success', 'message' => 'User removed from list successfully']);
+            }else{
+                $this->dispatchBrowserEvent('alert', ['type' => 'error', 'message' => 'Unable to remove from list.']);
+            }
+            
+        }else{
+            $this->dispatchBrowserEvent('alert', ['type' => 'error', 'message' => 'No such user existed in list.']);
+        }
+    }
+
+
+
+
+}
